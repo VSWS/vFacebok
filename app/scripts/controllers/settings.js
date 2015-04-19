1453ee5b5e180a19c082b9b7ccaad03b8c52dbe9
tungtouch <tungnguyen@vsoft.vn>
2015-04-19 10:45:25 +0700

    update

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -69,7 +69,6 @@ angular.module('ifacebookApp')
                             })
                         });
 
-
                     });
                     $scope.loggedIn = true;
                 } else {
